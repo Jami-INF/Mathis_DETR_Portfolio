fe8d0651d3cb6209695726400c14663dfa7e888a
Jami Infante <83696339+Jami-INF@users.noreply.github.com>
2026-06-16 14:10:46 +0200

    ADD CI Github pages

--- portfolio_mathis/astro.config.mjs
+++ portfolio_mathis/astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://jami-inf.github.io',
+  base: '/Mathis_DETR_Portfolio',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
