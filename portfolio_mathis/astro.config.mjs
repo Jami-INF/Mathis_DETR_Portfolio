ff6ee2d425598c32abb513c70e9c5a943ad9bc3e
Jami Infante <83696339+Jami-INF@users.noreply.github.com>
2026-06-24 21:01:13 +0200

    ADD : new domaine OVH

--- portfolio_mathis/astro.config.mjs
+++ portfolio_mathis/astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://jami-inf.github.io',
-  base: '/Mathis_DETR_Portfolio',
+  site: 'https://mathis-detroussat.fr',
   vite: {
     plugins: [tailwindcss()],
   },
